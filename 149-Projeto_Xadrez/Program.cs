ff2133ac5f8364466404b7884004e3deea7f0cf6
Pablo Victor Simões de Lima <pablosdlima@hotmail.com>
2020-04-04 15:15:46 -0300

    Tratamento de exceção Tabuleiro.[C

--- 149-Projeto_Xadrez/Program.cs
+++ 149-Projeto_Xadrez/Program.cs
@@ -13,13 +13,19 @@ namespace _149_Projeto_Xadrez
     {
         static void Main(string[] args)
         {
-            mdTabuleiro tabuleiro = new mdTabuleiro(8, 8);//tamanho do tabuleiro.
-
-            tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preto), new Posicao(0, 0)); //add peça em
-            tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preto), new Posicao(1, 3)); //add peça em
-            tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preto), new Posicao(2, 4)); //add peça em
-
-            Tela.imprimirTabuleiro(tabuleiro);
+            try
+            {
+                mdTabuleiro tabuleiro = new mdTabuleiro(8, 8);//tamanho do tabuleiro.
+                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preto), new Posicao(0, 0)); //add peça em
+                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preto), new Posicao(1, 3)); //add peça em
+                tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preto), new Posicao(2, 4)); //add peça em
+                Tela.imprimirTabuleiro(tabuleiro);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
 
             Console.ReadLine();
 
--- 149-Projeto_Xadrez/Tabuleiro/mdTabuleiro.cs
+++ 149-Projeto_Xadrez/Tabuleiro/mdTabuleiro.cs
@@ -25,10 +25,44 @@ namespace _149_Projeto_Xadrez.tabuleiro
             return pecas[nLinhas, nColunas];
         }
 
+        public Peca Peca(Posicao pos)//sobrecarga no metodo peca.
+        {
+            return pecas[pos.Linha, pos.Coluna];
+
+        }
+
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição.");
+            }
+
             pecas[pos.Linha, pos.Coluna] = p; //joga a peça P na posição indicada.
             p.Posicao = pos; //posição de p = pos
         }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return Peca(pos) != null;
+        }//verifica se a peça existe em determinada posição.
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha >= NLinhas || pos.Coluna < 0 || pos.Coluna >= NColunas)
+            {
+                return false;
+            }
+            return true;
+        }//testa a posição se é valida e retorna ao metodo acima.
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição Inválida!");
+            }
+        }//cria a exceção caso o metodo posicaoValida for false.
     }
 }
