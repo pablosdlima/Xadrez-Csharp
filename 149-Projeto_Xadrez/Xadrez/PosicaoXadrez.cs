be224e16e8fc082995cd19591e2f40e2d53c45fe
Pablo Victor Simões de Lima <pablosdlima@hotmail.com>
2020-04-04 15:32:17 -0300

    Add classe Posicao Xadrez.

---
+++ 149-Projeto_Xadrez/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,31 @@
+﻿using _149_Projeto_Xadrez.Tabuleiro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _149_Projeto_Xadrez.Xadrez
+{
+    class PosicaoXadrez
+    {
+        public char PxColuna { get; set; }
+        public char PxLinha { get; set; }
+
+        public PosicaoXadrez(char pxColuna, char pxLinha)
+        {
+            PxColuna = pxColuna;
+            PxLinha = pxLinha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - PxLinha, PxColuna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return " " + PxColuna + PxLinha;
+        }//retorna em texto. metodo tostring
+    }
+}
