d81ed065b816e614b9b964b0aa607e027e55baf2
Pablo Victor Simões de Lima <pablosdlima@hotmail.com>
2020-04-22 17:03:44 -0300

    jogada especial Roque 176

--- 149-Projeto_Xadrez/Xadrez/Rei.cs
+++ 149-Projeto_Xadrez/Xadrez/Rei.cs
@@ -10,9 +10,11 @@ namespace _149_Projeto_Xadrez.Xadrez
 {
     class Rei : Peca
     {
-        public Rei(mdTabuleiro tabuleiro, Cor cor) : base(tabuleiro, cor) 
-        {
+        private PartidaDeXadrez partida;
 
+        public Rei(mdTabuleiro tabuleiro, Cor cor, PartidaDeXadrez partida) : base(tabuleiro, cor) 
+        {
+            this.partida = partida;
         }
 
         public override string ToString()
@@ -24,6 +26,12 @@ namespace _149_Projeto_Xadrez.Xadrez
         {
             Peca p = tabuleiro.Peca(pos.Linha, pos.Coluna);
             return p == null || p.cor != this.cor;
+        }
+
+        private bool testeTorreParaRoque(Posicao pos)
+        {
+            Peca p = tabuleiro.Peca(pos);
+            return p != null && p is Torre && p.cor == cor && p.qteMovimentos == 0;
         }
 
         public override bool[,] movimentosPossiveis()
@@ -67,6 +75,35 @@ namespace _149_Projeto_Xadrez.Xadrez
             {
                 mat[pos.Linha, pos.Coluna] = true;
             }//no
+
+            if(qteMovimentos == 0 && !partida.xeque)
+            {
+                Posicao posT1 = new Posicao(posicao.Linha, posicao.Coluna + 3);
+                if (testeTorreParaRoque(posT1))
+                {
+                    Posicao p1 = new Posicao(posicao.Linha, posicao.Coluna + 1);
+                    Posicao p2 = new Posicao(posicao.Linha, posicao.Coluna + 2);
+
+                    if(tabuleiro.Peca(p1) == null && tabuleiro.Peca(p2) == null)
+                    {
+                        mat[posicao.Linha, posicao.Coluna + 2] = true;
+                    }
+                }//jogada especial, ROQUE PEQUENO
+
+                Posicao posT2 = new Posicao(posicao.Linha, posicao.Coluna - 4);
+                if (testeTorreParaRoque(posT2))
+                {
+                    Posicao p1 = new Posicao(posicao.Linha, posicao.Coluna - 1);
+                    Posicao p2 = new Posicao(posicao.Linha, posicao.Coluna - 2);
+                    Posicao p3 = new Posicao(posicao.Linha, posicao.Coluna - 3);
+
+                    if (tabuleiro.Peca(p1) == null && tabuleiro.Peca(p2) == null && tabuleiro.Peca(p3) == null)
+                    {
+                        mat[posicao.Linha, posicao.Coluna - 2] = true;
+                    }
+                }//jogada especial, ROQUE grande
+
+            }
 
             return mat;
         }
